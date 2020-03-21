7a2408abe6a27812f07d78f0328f7c274371d099
Alexandr <ans11081990@gmail.com>
2020-03-21 22:50:15 +0300

    init: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (bracketsConfig.some(item => str.includes(item.join('')))) {
+    bracketsConfig.forEach(item => {
+      str = str.replace(item.join(''), '');
+    });
+  }
+  return str ? false : true;
 }
